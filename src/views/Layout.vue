0d0a24b561fd1d9ef3ac53d992795e0ab4238332
闻紫阳 <wenzy@orderhandler.com>
2019-04-09 16:11:57 +0800

    结构 调整

---
+++ src/views/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="layout">
+		<div class="left">
+			left
+		</div>
+
+		<div class="right">
+			<router-view name="main"/>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Layout"
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.layout
+		display flex
+		.left
+			flex 0 0 300px
+			background #ff000f
+		.right
+			flex 1
+</style>
